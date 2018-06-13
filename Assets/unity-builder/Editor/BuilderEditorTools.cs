80fe07ef72c4edf7796fd1b037ce1fd2e8961b9d
Ben Skinner <benjamin.skinner96@gmail.com>
2018-06-13 21:21:38 +1000

    Fixed things for v0.2

--- Assets/unity-builder/Editor/BuilderEditorTools.cs
+++ Assets/unity-builder/Editor/BuilderEditorTools.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEditor;
+using System.IO;
 
 public class BuilderEditorTools : MonoBehaviour {
 
@@ -16,13 +17,17 @@ public class BuilderEditorTools : MonoBehaviour {
 		
 	}
 
-	[MenuItem("Builder/Create/BuildPreferences")]
+	[MenuItem("Builder/Create Build Preferences", priority = 0)]
 	public static void CreateBuildPreferences()
 	{
 		BuildPreferences asset = ScriptableObject.CreateInstance<BuildPreferences>();
+        
 
-		AssetDatabase.CreateFolder ("Assets", "Resources");
-		AssetDatabase.CreateFolder ("Assets/Resources", "Preferences");
+        if(!AssetDatabase.IsValidFolder("Assets/Resources"))
+            AssetDatabase.CreateFolder ("Assets", "Resources");
+        if (!AssetDatabase.IsValidFolder("Assets/Resources/Preferences"))
+            AssetDatabase.CreateFolder ("Assets/Resources", "Preferences");
+
 		AssetDatabase.CreateAsset(asset, "Assets/Resources/Preferences/BuildPrefs.asset");
 		AssetDatabase.SaveAssets();
 
@@ -30,13 +35,8 @@ public class BuilderEditorTools : MonoBehaviour {
 
 		Selection.activeObject = asset;
 	}
-
-	[MenuItem("Builder/Build/Android")]
-	public static void BuildAndroid() {
-		BuildAny (BuildTarget.Android);
-	}
-
-	[MenuItem("Builder/Build/All Project Targets", false, 0)]
+    
+	[MenuItem("Builder/Build/Build Development Targets", priority = 1)]
 	public static void BuildAllProjectTargets() {
 		BuildPreferences prefs = Resources.Load<BuildPreferences>("Preferences/BuildPrefs");
 
@@ -58,9 +58,18 @@ public class BuilderEditorTools : MonoBehaviour {
 		buildPlayerOptions.scenes = prefs.ScenePaths;
 
 		Debug.Log (buildPlayerOptions.scenes);
+        
+        buildPlayerOptions.locationPathName = string.Format("{0}\\{3}\\{2}\\{3}_{2}_{1}{4}",
+            Path.GetFullPath(Application.dataPath + "/../" + prefs.BuildFolder), 
+            prefs.ProjectName,
+            prefs.ProjectVersion,
+            platformName,
+            ResolveExtension(a_platform)
+        );
 
-		buildPlayerOptions.locationPathName = string.Format("/{0}/{3}/{2}/{1}_{2}_Android.apk", Application.dataPath + "/../" + prefs.BuildFolder, prefs.ProjectName, prefs.ProjectVersion, platformName);
-		Debug.Log (buildPlayerOptions.locationPathName);
+        Debug.Log(buildPlayerOptions.locationPathName);
+        //{1}_{2}_Android.apk
+        Debug.Log (buildPlayerOptions.locationPathName);
 		buildPlayerOptions.target = a_platform;
 
 		BuildOptions bo = new BuildOptions();
@@ -73,5 +82,28 @@ public class BuilderEditorTools : MonoBehaviour {
 
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
 	}
+
+    static string ResolveExtension(BuildTarget a_target)
+    {
+        switch(a_target) {
+            case BuildTarget.StandaloneOSX:
+                return ".app";
+
+            case BuildTarget.StandaloneWindows:
+            case BuildTarget.StandaloneWindows64:
+                return ".exe";
+
+            case BuildTarget.iOS:
+                return ".xcode";
+                    
+            case BuildTarget.Android:
+                return ".apk";
+
+            case BuildTarget.WebGL:
+                return ".html";
+        }
+
+        return "";
+    }
 
 }
